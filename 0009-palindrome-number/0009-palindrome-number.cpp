17fc17a22a9fb4e69afb7cca3c15f54e170f22a5
Raj Aryan <aryanraj212005@gmail.com>
2026-08-08 09:49:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.14%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0) {
+            return false;
+        }
+
+        long long r = 0;
+        int original = x;
+        while (x>0) {
+            r = r*10 + x%10;
+            x=x/10;
+        }
+        return r==original;
+    }
+};
